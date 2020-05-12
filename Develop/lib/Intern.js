52f6ace905b88ecd4468d8c04621bdf7ac026ff2
AFeese <58886725+AFeese@users.noreply.github.com>
2020-05-12 16:00:53 -0400

    passed test on Intern.js

--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,2 +1,22 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
 //Intern to also have: *school, *getSchool(), and *getRole() // Overridden to return 'Intern'
+
+const Employee = require("./Employee");
+
+class Intern extends Employee {
+    constructor(name, id, email, school) {
+        super(name, id, email);
+        this.school = school;
+    }
+
+    getSchool(){
+        return this.school;
+    }
+
+    getRole(){
+        return Intern.name;
+        //or return Intern.name; ?//
+    }
+}
+
+module.exports = Intern;

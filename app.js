6b96740dfea7c3bc89d940e128f2129749e4447d
AFeese <58886725+AFeese@users.noreply.github.com>
2020-05-14 11:03:55 -0400

    added comments to app.js for clear direction of code.

--- app.js
+++ app.js
@@ -19,11 +19,16 @@ const render = require("./lib/htmlRenderer");
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
-const empArray = [];
+//Empty array holds each employee type's information after user has entered the information into the prompts.
+const teamArray = [];
 
+//Created variable to hold the answers from user selection of employee type
 let teamSelection;
 
+//First function to run starts the selection:
 teamMemberOptions();
+
+//Team member selection formed here:
 function teamMemberOptions() {
   inquirer.prompt({
     type: "list",
@@ -35,6 +40,7 @@ function teamMemberOptions() {
       "Intern",
       "Done"
     ]
+    //Allows us to see all answers collected from the teamMemeberOptions object:
   }).then(answers => {
     teamSelection = answers;
     console.log(answers);
@@ -43,6 +49,7 @@ function teamMemberOptions() {
     // information; write your code to ask different questions via inquirer depending on
     // employee type.
 
+    //First prompts for manager
     if (teamSelection.team === 'Manager') {
       inquirer.prompt([
         {
@@ -66,13 +73,16 @@ function teamMemberOptions() {
           message: "What is the manager's office number?"
         }
 
+        //takes the answers (user input) from manager object and stores them to communicate with the Class:
       ]).then(function (answers) {
         const manager = new Manager(answers.name, answers.id, answers.email, answers.office);
 
+        //pushes answers from Manager to the employee array
         console.log(manager);
-        empArray.push(manager);
+        teamArray.push(manager);
         teamMemberOptions();
       })
+        //catches any errors with function
         .catch(function (err) {
           console.log(err);
         })
@@ -81,6 +91,7 @@ function teamMemberOptions() {
       // information; write your code to ask different questions via inquirer depending on
       // employee type.
 
+      //Prompts for Engineer
     } else if (teamSelection.team === 'Engineer') {
       inquirer.prompt([
         {
@@ -104,13 +115,16 @@ function teamMemberOptions() {
           message: "What is the Engineer's GitHub username?"
         }
 
+        //takes the answers (user input) from manager object and stores them to communicate with the Class:
       ]).then(function (answers) {
         const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
 
+        //pushes answers from Engineer to team array
         console.log(engineer);
-        empArray.push(engineer);
+        teamArray.push(engineer);
         teamMemberOptions();
       })
+        //catches any errors from function
         .catch(function (err) {
           console.log(err);
         })
@@ -119,6 +133,7 @@ function teamMemberOptions() {
       // information; write your code to ask different questions via inquirer depending on
       // employee type.
 
+      //prompts for Intern
     } else if (teamSelection.team === 'Intern') {
       inquirer.prompt([
         {
@@ -142,27 +157,36 @@ function teamMemberOptions() {
           message: "What school does your Intern attend?"
         }
 
+        //takes the answers (user input) from manager object and stores them to communicate with the Class:
       ]).then(function (answers) {
         const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
 
+        //pushes answers from Intern object to team array
         console.log(intern);
-        empArray.push(intern);
+        teamArray.push(intern);
         teamMemberOptions();
       })
+
+        //catches any errors from function
         .catch(function (err) {
           console.log(err);
         })
 
+      //condition for if the user selects "Done" to end the prompts
     } else if (teamSelection.team === "Done") {
       // After the user has input all employees desired, call the `render` function (required
       // above) and pass in an array containing all employee objects; the `render` function will
       // generate and return a block of HTML including templated divs for each employee!
-      let html = render(empArray);
+
+      //Calling the render function with the information we pushed to the teamArray, sends it to the HTML.
+      let html = render(teamArray);
       // After you have your html, you're now ready to create an HTML file using the HTML
       // returned from the `render` function. Now write it to a file named `team.html` in the
       // `output` folder. You can use the variable `outputPath` above target this location.
       // Hint: you may need to check if the `output` folder exists and create it if it
       // does not.
+
+      //Writes the HMTL file containing all employees entered to the outputPath folder, makes the team.html page.
       fs.writeFile(outputPath, html, function (err) {
 
         if (err) {
